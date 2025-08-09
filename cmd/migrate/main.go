65a8b1fa7ab9241d3e5aded3b4e90a6f5ab29035
Junior <eecfredes@gmail.com>
2025-08-08 22:07:24 -0300

    :tada:Event api

---
+++ cmd/migrate/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/database/sqlite3"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a migration direction: 'up' or 'down'")
+	}
+
+	direction := os.Args[1]
+
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	instance, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module event-app
+
+go 1.24.5
+
+require (
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/mattn/go-sqlite3 v1.14.30 // indirect
+)
