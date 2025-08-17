061539358983a602e6feeead55bb01b505c699ff
Junior <eecfredes@gmail.com>
2025-08-16 21:36:40 -0300

    :rocket:handler create event

---
+++ cmd/api/events.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"event-app/internal/database"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (app *application) createEvent(c *gin.Context) {
+	var event database.Events
+
+	if err := c.ShouldBindJSON(&event); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err := app.models.Events.insert(&event)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "❌ Failed to create event"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, event)
+}
