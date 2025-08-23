f9eecf01d5ed7e31126aaa9d8b89f3fabc0b0343
Junior <eecfredes@gmail.com>
2025-08-22 22:01:53 -0300

    :rocket:Auth

---
+++ cmd/api/auth.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type registerRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+	Name     string `json:"name" binding:"required,min=2"`
+}
+
+func (app *application) registerUser(c *gin.Context) {
+
+}
