15b265a0fbe1a6250ac2fb55840ad9fc4ffcb864
Junior <eecfredes@gmail.com>
2025-08-23 22:58:57 -0300

    :rocket:Register user function

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"event-app/internal/database"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type registerRequest struct {
 	Email    string `json:"email" binding:"required,email"`
@@ -9,5 +15,31 @@ type registerRequest struct {
 }
 
 func (app *application) registerUser(c *gin.Context) {
+	var register registerRequest
 
+	if err := c.ShouldBindJSON(&register); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(register.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "❌ Somethig went wrong"})
+		return
+	}
+
+	register.Password = string(hashedPassword)
+	user := database.User{
+		Email:    register.Email,
+		Password: register.Password,
+		Name:     register.Name,
+	}
+
+	err = app.models.Users.insert(&user)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "❌ Could not create user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, user)
 }
