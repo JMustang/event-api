f6d893853f9ad28311238d7048991b71cc988bcd
Junior <eecfredes@gmail.com>
2025-08-28 22:41:43 -0300

    :rocket:Insert model

--- internal/database/attendees.go
+++ internal/database/attendees.go
@@ -1,13 +1,31 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type AttendeeModel struct {
 	DB *sql.DB
 }
 
-type Attendees struct {
+type Attendee struct {
 	Id      int `json:"id"`
 	UserId  int `json:"userId"`
 	EventId int `json:"eventId"`
+}
+
+func (m *AttendeeModel) Insert(attendee *Attendee) (*Attendee, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "INSERT INTO attendees (event_id, user_id) VALUES ($1, $2) RETURNING id"
+	err := m.DB.QueryRowContext(ctx, query, attendee.EventId, attendee.UserId).Scan(&attendee.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return attendee, nil
 }
