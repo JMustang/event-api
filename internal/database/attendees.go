28e6d779a018acb56e94d3806acea5bf8e3b2465
Junior <eecfredes@gmail.com>
2025-08-29 21:46:20 -0300

    :rocket:Get By Event And Attendee, function

--- internal/database/attendees.go
+++ internal/database/attendees.go
@@ -29,3 +29,22 @@ func (m *AttendeeModel) Insert(attendee *Attendee) (*Attendee, error) {
 
 	return attendee, nil
 }
+
+func (m *AttendeeModel) GetByEventAndAttendee(eventId, userId int) (*Attendee, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "SELECT * FROM attendees WHERE event_id = $1 AND user_id = $2"
+
+	var attendee Attendee
+	err := m.DB.QueryRowContext(ctx, query, eventId, userId).Scan(&attendee.Id, &attendee.UserId, &attendee.EventId)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return &attendee, nil
+}
