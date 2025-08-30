a2105906f81669aff1a748f510fcab8dfe7797b1
Junior <eecfredes@gmail.com>
2025-08-30 20:05:20 -0300

    :rocket:Get Attendees By Event, model

--- internal/database/attendees.go
+++ internal/database/attendees.go
@@ -48,3 +48,36 @@ func (m *AttendeeModel) GetByEventAndAttendee(eventId, userId int) (*Attendee, e
 
 	return &attendee, nil
 }
+
+func (m *AttendeeModel) GetAttendeesByEvent(eventId int) ([]*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `
+		SELECT u.id, u.name, u.email
+		FROM users u
+		JOIN attendees a ON u.id = a.user_id
+		WHERE a.event_id = $1
+	`
+
+	rows, err := m.DB.QueryContext(ctx, query, eventId)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var users []*User
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.Id, &user.Name, &user.Email)
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, &user)
+	}
+
+	return users, nil
+}
